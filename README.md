31ba9375b410bec57e1636185d52c451e93382e6
DummyJack <s38958091@gmail.com>
2024-10-30 16:47:40 +0800

    加上bash、更新README

--- README.md
+++ README.md
@@ -2,6 +2,27 @@
 
 ## 姓名：廖書賢
 
+## 設置
+
+_Requires Java 11 or greater._
+
+### 啟動
+
+### Mac
+
+```bash
+chmod +x run_test.sh
+./run_test.sh
+```
+
+### 測試連結(本地)
+
+1. [網頁](http://localhost:6789)
+
+2. [JPEG](http://localhost:6789/test.jpeg)
+
+3. [GIF](http://localhost:6789/test.gif)
+
 ## 補充
 
 ### [題目連結](http://www2.ic.uff.br/~michael/kr1999/9-appendix/9_01-WebServer.html)
---
+++ run_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 檢查 Java 是否安裝
+echo -e "${BLUE}檢查 Java 環境...${NC}"
+if ! command -v java &> /dev/null; then
+    echo "錯誤: 需要安裝 Java"
+    exit 1
+fi
+
+# 編譯 Java 文件
+echo -e "編譯 Java 文件..."
+javac -d bin src/WebServer.java
+
+# 檢查編譯是否成功
+if [ $? -ne 0 ]; then
+    echo "編譯失敗"
+    exit 1
+fi
+
+# 啟動服務器
+echo -e "訪問 http://localhost:6789"
+echo -e "按 Ctrl+C 停止服務器"
+
+# 運行服務器
+java -cp bin WebServer
